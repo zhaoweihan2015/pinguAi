780f2599e1e475b4dc549d8bbe2b04d3ebf836d1
赵玮涵 <zhaoweihan@troila.com>
2025-03-21 16:48:10 +0800

    feat: scroll style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,14 @@ import { ConfigProvider } from "antd";
 import { GlobalProvider } from "@/lib/hooks/useGlobal";
 const RootLayout = ({ children }: React.PropsWithChildren) => (
   <html lang="en">
+    <head>
+      <meta name="viewport"
+        content="width=device-width,
+               initial-scale=1.0,
+               maximum-scale=1.0,
+               viewport-fit=cover"
+      />
+    </head>
     <body>
       <ConfigProvider
         theme={{
