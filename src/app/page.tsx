1646b8bd7d7b7839643a182cb1dcf1522705a2b5
赵玮涵 <zhaoweihan@troila.com>
2025-05-28 16:18:05 +0800

    feat: 设置标题

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Chat from "@/components/Chat";
+import { Metadata } from "next";
 import { App } from "antd";
+import Chat from "@/components/Chat";
+
+export const metadata: Metadata = {
+  title: "pingu AI",
+  description: "pingu AI",
+};
 
 export default function Home() {
   return (
