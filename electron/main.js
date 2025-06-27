2a5b8500639b014442002d805b4a137355495ed5
DESKTOP-FC9UL68\Gua <@zhaoweihan>
2025-06-27 16:47:07 +0800

    feat: add electron webview

---
+++ electron/main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { fork } = require('child_process');
+const isDev = !app.isPackaged;
+
+const port = 3000;
+
+async function createWindow() {
+  console.log(isDev)
+  console.log("--------------------------------")
+
+  if (!isDev) await fork(path.join(app.getAppPath(), 'server.js'));
+
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+    },
+  });
+
+  
+  win.loadURL(`http://localhost:${port}`);
+  
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  // 关闭 server.js 子进程
+  if (global.serverProcess && !global.serverProcess.killed) {
+    global.serverProcess.kill();
+  }
+
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
---
+++ server.js
@@ -0,0 +1,21 @@
+const http  = require('http');
+const { parse } = require('url');
+const next  = require('next');
+
+const port = process.env.PORT || 3000;    
+const dev  = false;                       
+
+
+const app = next({ dev, dir: __dirname });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  http
+    .createServer((req, res) => {
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl);
+    })
+    .listen(port, '127.0.0.1', () => {
+      console.log(`> Next.js server ready at http://localhost:${port}`);
+    });
+});
